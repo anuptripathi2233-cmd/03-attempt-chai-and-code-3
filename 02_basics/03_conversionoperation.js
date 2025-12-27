d22774987fe6f19042ca344edda0288fa539f594
anuptripathi2233-cmd <anuptripathi2233@gmail.com>
2025-12-27 17:46:46 +0000

    conversion of datatype

---
+++ 02_basics/03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = null
+
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+ let valueInNumber = Number(score)
+//   console.log(typeof valueInNumber);
+//   console.log(valueInNumber);
+
+
+  //"33" => 33
+  // "33" => NaN
+  // TRUE =>1; FALSE=>0
+
+  let isloggedIn = 1
+
+  let booleanisloggedIn = Boolean(isloggedIn)
+ // console.log(booleanisloggedIn);
+
+  //1=> true, 0=> false
+  //""=> false
+  // "hitesh"=> true
+
+  let someNumber = 33
+
+  let stringNumber = String(someNumber)
+  console.log(stringNumber);
+  console.log(typeof stringNumber);
